0745df3a06991fd67e0ed6acdcae087a7b2d57f0
Auriel-Wish <44419323+Auriel-Wish@users.noreply.github.com>
2023-07-09 15:21:43 +0300

    access token is good. Working on scope issues. In theory the req file is also good

--- req.js
+++ req.js
@@ -1,24 +1,32 @@
 const axios = require('axios');
 const getAccessToken = require('./accessToken');
+var headers = {Authorization:0, orgId:0};
 
 getAccessToken()
     .then(accessToken => {
-        // console.log(accessToken);
-        const url = 'https://desk.zoho.com/api/v1/tickets';
+        let urlTickets = 'https://desk.zoho.com/api/v1/tickets';
         // const url = 'https://desk.zoho.com/api/v1/agents';
-        const headers = {
+        headers = {
             Authorization: `Zoho-oauthtoken ${accessToken}`,
             orgId: '807931464',
         };
 
-        return axios.get(url, { headers });
+        return axios.get(urlTickets, { headers });
     })
     .then(response => {
-        for (let i = 0; i < response.data.data.length; i++) {
-            console.log(response.data.data[i].assigneeId);
-        }
+        // for (let i = 0; i < response.data.data.length; i++) {
+        //     console.log(response.data.data[i].assigneeId);
+        // }
+    })
+    .then(accessToken => {
+        const urlAgents = 'https://desk.zoho.com/api/v1/agents';
+
+        return axios.get(urlAgents, { headers });
+    })
+    .then(response => {
+        console.log(response.data);
     })
     .catch(error => {
-        console.error('Error:', error);
+        console.error('ERROR:\n' + error.response.data.errorCode + '\n' + error.response.data.message);
         // Handle any errors that occur during the request
     });
