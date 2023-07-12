9fcbf56bccaa5e4e949faa9c8b24ed15acc339a9
Auriel-Wish <44419323+Auriel-Wish@users.noreply.github.com>
2023-07-12 11:49:59 +0300

    fully functional Auth Token script. req.js matches the agents and the tickets based on the IDs

--- accessToken.js
+++ accessToken.js
@@ -1,24 +1,40 @@
 const axios = require('axios');
+const config = require('./config');
 
 function getAccessToken() {
-  const url = 'https://accounts.zoho.com/oauth/v2/token';
-  const refreshToken = '1000.bd8fdd262d7a5f3fd1c0dc7f85c9070c.1a9102296dd26265a771d0df5057b0b0';
-  const clientID = '1000.K8ZBYBC2TPZTJTZG5R25CR7C2735WH';
-  const clienSecret = '0fecf52eb7f38ce0420bbb53a7d7fd0aebedd530f9';
-  const scope = 'Desk.tickets.ALL,Desk.settings.READ,Desk.basic.READ';
-  const grantType = 'refresh_token';
-  const fullUrl = `${url}?refresh_token=${refreshToken}&client_id=${clientID}&client_secret=${clienSecret}&scope=${scope}&grant_type=${grantType}`;
-    // console.log(fullUrl);
-  return axios.post(fullUrl)
-    .then(response => {
-      let accessToken = response.data.access_token;
-      console.log(accessToken);
-      return accessToken;
-    })
-    .catch(error => {
-      console.error(error);
-      throw error;
-    });
+    let url = 'https://accounts.zoho.com/oauth/v2/token';
+    let grantType = 'refresh_token';
+    let fullUrl = `${url}?refresh_token=${config.refreshToken}&client_id=${config.clientID}&client_secret=${config.clientSecret}&scope=${config.scope}&grant_type=${grantType}`;
+
+    return axios.post(fullUrl)
+        .then(response => {
+            let accessToken = response.data.access_token;
+            // console.log(accessToken);
+            return accessToken;
+        })
+        .catch(error => {
+          console.error(error);
+          throw error;
+        });
 }
 
-module.exports = getAccessToken;
+function createOriginalTokens() {
+    let url = 'https://accounts.zoho.com/oauth/v2/token';
+    let grantType = 'authorization_code';
+    let fullUrl = `${url}?code=${config.code}&grant_type=${grantType}&client_id=${config.clientID}&client_secret=${config.clientSecret}`;
+
+    return axios.post(fullUrl)
+        .then(response => {
+            console.log(response.data);
+            console.log(response.data.refresh_token);
+        })
+        .catch(error => {
+          console.error(error);
+          throw error;
+        });
+}
+
+module.exports = {
+    getAccessToken,
+    createOriginalTokens
+};
--- req.js
+++ req.js
@@ -1,32 +1,52 @@
 const axios = require('axios');
-const getAccessToken = require('./accessToken');
+const tokenFuncs = require('./accessToken');
+const config = require('./config');
 var headers = {Authorization:0, orgId:0};
 
-getAccessToken()
+// tokenFuncs.createOriginalTokens();
+var assigneeIDs = [];
+var agentIDs = {};
+
+tokenFuncs.getAccessToken()
     .then(accessToken => {
         let urlTickets = 'https://desk.zoho.com/api/v1/tickets';
-        // const url = 'https://desk.zoho.com/api/v1/agents';
         headers = {
             Authorization: `Zoho-oauthtoken ${accessToken}`,
-            orgId: '807931464',
+            orgId: config.orgID,
         };
 
         return axios.get(urlTickets, { headers });
     })
     .then(response => {
-        // for (let i = 0; i < response.data.data.length; i++) {
-        //     console.log(response.data.data[i].assigneeId);
-        // }
+        for (let i = 0; i < response.data.data.length; i++) {
+            // console.log(response.data.data[i].assigneeId);
+            assigneeIDs.push(response.data.data[i].assigneeId);
+        }
     })
-    .then(accessToken => {
-        const urlAgents = 'https://desk.zoho.com/api/v1/agents';
-
+    .then(() => {
+        let urlAgents = 'https://desk.zoho.com/api/v1/agents';
         return axios.get(urlAgents, { headers });
     })
     .then(response => {
-        console.log(response.data);
+        // console.log(response.data);
+        for (let i = 0; i < response.data.data.length; i++) {
+            // console.log(response.data.data[i].id);
+            let newAgent = {name: response.data.data[i].name, shortID:i, numTickets: 0};
+            agentIDs[response.data.data[i].id] = newAgent;
+        }
     })
     .catch(error => {
         console.error('ERROR:\n' + error.response.data.errorCode + '\n' + error.response.data.message);
-        // Handle any errors that occur during the request
-    });
+    })
+    .then(() => {
+        for (let i = 0; i < assigneeIDs.length; i++) {
+            if (assigneeIDs[i] in agentIDs) {
+                // console.log(`MATCH: ${assigneeIDs[i]}`);
+                (agentIDs[assigneeIDs[i]]).numTickets++;
+            }
+        }
+        console.log(agentIDs);
+        
+        // console.log('Agent IDs: ' + agentIDs);
+        // console.log('Assignee IDs: ' + assigneeIDs);
+    })
