1a1608a369da1b09918d06c37d79734f8fcfa900
Auriel-Wish <44419323+Auriel-Wish@users.noreply.github.com>
2023-07-24 12:56:00 +0300

    Added feature that equally distributes tickets to chosen agents after the all the agents with priority are full

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	code: '1000.edb9548a0cc9bc2173bb671df3c26c85.f003c605fbd8c72a4d100320fff20472',
 	refreshToken: '1000.7704f757838b8b0930d6f1cbfbc7b757.f30b48a9fdc3eb0519ef9f53aa3ede97',
-	accessToken: '1000.83b19dea2a7a24c6de01917ec89ecfb3.33ba4cd61bc52324b86827d9453bcc4a',
-	authTokenExpiration: 1689682707144,
+	accessToken: '1000.6feccf3749dc6ed0d244d2d7f4356eff.643f1718f5622bec3f4873959ff3d726',
+	authTokenExpiration: 1690194293749,
 	clientID: '1000.2WRU29VLNMXK3WYZVLMLAHGFRZGT5D',
 	clientSecret: 'e63f27e480e4052386a2b494c2af0652fa0aa66295',
 	scope: 'Desk.tickets.ALL,Desk.tickets.UPDATE,Desk.basic.READ,Desk.settings.READ,Desk.search.READ',
--- main.js
+++ main.js
@@ -17,12 +17,12 @@ var agents = {};
 var agentsDummy = {};
 var unassignedTickets = [];
 var priorityQueue = [];
+var groupedPriority = [];
 var allTickets = [];
 
 var wbName = 'zoho.xlsx';
 var wb = XLSX.readFile(wbName);
-var sheetName = wb.SheetNames[0];
-var sheet = wb.Sheets[sheetName];
+var sheet = wb.Sheets['Main'];
 
 // Driver function
 async function main() {
@@ -43,16 +43,22 @@ async function main() {
             // The key is their Zoho ID
             // shortID is an easier way to refer to them - it is designated when
             // the function runs
+            let id = 0;
             for (let i = 0; i < response.data.data.length; i++) {
                 let newAgent = {
                     name: response.data.data[i].name,
-                    shortID:i,
+                    shortID: id,
                     numTickets: 0,
                     numWaiting: 0,
                     maxTickets: 0,
                     maxWaiting: 0
                 };
-                agents[response.data.data[i].id] = newAgent;
+
+                // Leave out these people/entities - they don't get tickets but they are in Zoho
+                if (newAgent.name != 'Customer Service' && newAgent.name != 'Shachar  Gafni' && newAgent.name != 'Shachar Gafni') {
+                    agents[response.data.data[i].id] = newAgent;
+                    id++;
+                }
             }
             
             // Order the agents
@@ -117,39 +123,64 @@ function placeTicket(ticket) {
     // Ticket will get assigned unless no agent has room for it
     while (priorityQueue.length > 0 && !ticketAllocated) {
         for (const [id, agent] of Object.entries(agents)) {
-            // Assign the ticket to the first agent on the queue
-            if (agent.shortID == priorityQueue[0]) {
-                // Assign it to them as long as they have space
-                if (agent.numTickets < agent.maxTickets && agent.numWaiting < agent.maxWaiting) {                    
-                    let ticketData = {
-                        assigneeId: id
-                    };
-
-                    let subject = ticket.subject;
-                    console.log(`Assigning ticket to ${agent.name}`);
-                    console.log(`Subject: ${subject}\n`);
-
-                    // Assign the ticket
-                    // updateTicketUrl = `https://desk.zoho.com/api/v1/tickets/${ticket.id}`;
-                    // axios.patch(updateTicketUrl, ticketData, { headers })
-                    //     .then(() => {})
-                    //     .catch(error => {
-                    //         console.error('ERROR:\n' + error.response.data.errorCode + '\n' + error.response.data.message);
-                    //     });
-
-                    // Document that the ticket has been assigned
-                    agent.numTickets++;
-                    if (ticket.status.toLowerCase() == 'waiting on us') {
-                        agent.numWaiting++;
-                    }
+            // Check if the queue is still priority-based
+            if (priorityQueue[0] == groupedPriority && groupedPriority[0] == agent.shortID) {
+                if (agent.numTickets < agent.maxTickets && agent.numWaiting < agent.maxWaiting) {
+                    // Assign tickets equally to everyone left
+                    assignTicket(id, ticket, agent);
+                    groupedPriority.push(groupedPriority[0]);
+                    groupedPriority.shift();
                     ticketAllocated = true;
+                    break;
                 }
-                // If the agent has no space for more tickets, remove them from the queue
                 else {
+                    groupedPriority.shift();
+                }
+                if (groupedPriority.length == 0) {
                     priorityQueue.shift();
                 }
             }
+            else {
+                // Assign the ticket to the first agent on the queue
+                if (agent.shortID == priorityQueue[0]) {
+                    // Assign it to them as long as they have space
+                    if (agent.numTickets < agent.maxTickets && agent.numWaiting < agent.maxWaiting) {                    
+                        assignTicket(id, ticket, agent);
+                        ticketAllocated = true;
+                        break;
+                    }
+                    // If the agent has no space for more tickets, remove them from the queue
+                    else {
+                        priorityQueue.shift();
+                    }
+                }
+            }
         }
+    }
+}
+
+// API call to assign the ticket
+function assignTicket(id, ticket, agent) {
+    let ticketData = {
+        assigneeId: id
+    };
+
+    let subject = ticket.subject;
+    console.log(`Assigning ticket to ${agent.name}`);
+    console.log(`Subject: ${subject}\n`);
+
+    // Assign the ticket
+    updateTicketUrl = `https://desk.zoho.com/api/v1/tickets/${ticket.id}`;
+    axios.patch(updateTicketUrl, ticketData, { headers })
+        .then(() => {})
+        .catch(error => {
+            console.error(error);
+        });
+
+    // Document that the ticket has been assigned
+    agent.numTickets++;
+    if (ticket.status.toLowerCase() == 'waiting on us') {
+        agent.numWaiting++;
     }
 }
 
@@ -169,7 +200,7 @@ function sayNumTickets() {
                     t: 'n',
                     v: agent.numTickets
                 }
-                sheet[`F${i + 1}`] = newCell;
+                sheet[`G${i + 1}`] = newCell;
             }
         }
     }
@@ -181,22 +212,43 @@ function sayNumTickets() {
 function createPriorityQueue() {
     let x = 0;
     let newCell;
+    let restartProgram = false;
     // Write in all of the agents and their IDs (in case there has been an update
     // to the agents)
     Object.keys(agents).forEach(function(id, agent) {
-        newCell = {
-            t: 's',
-            v: agents[id].name
+        // 2 different ways of updating cells
+        // Necessary because inserting a non-existent cell is different
+        // than editing an existing cell
+        
+        // Cell with name
+        let cellRef = XLSX.utils.encode_cell({c: 0, r: x + 1});
+        let cell = sheet[cellRef];
+        if (cell) {
+            // update existing cell
+            cell.v = agents[id].name;
+        } else {
+            // add new cell
+            XLSX.utils.sheet_add_aoa(sheet, [[agents[id].name]], {origin: cellRef});
+            restartProgram = true;
+            console.log(`A new agent has been added: ${agents[id].name}\nPlease fill in the necessary cells and run again.`);
         }
-        sheet[`A${x + 2}`] = newCell;
+
+        // Cell with ID
         newCell = {
-            t: 'n',
-            v: agents[id].shortID
+            v: agents[id].shortID,
+            t: 'n'
         }
         sheet[`B${x + 2}`] = newCell;
         x++;
     });
+
     XLSX.writeFile(wb, wbName);
+
+    // Error checking
+    if (restartProgram) {
+        process.exit(1);
+    }
+    checkOrderedIDs();
 
     // Read in the agent ticket limitations and assign it to the agents
     let data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
@@ -204,11 +256,13 @@ function createPriorityQueue() {
     let maxTickets;
     let maxWaiting;
     let currID;
+    let hasPriority;
     for (let i = 1; i < data.length; i++) {
         currData = data[i];
         currID = currData[2];
-        maxTickets = currData[3];
-        maxWaiting = currData[4];
+        hasPriority = currData[3];
+        maxTickets = currData[4];
+        maxWaiting = currData[5];
 
         for (const [id, agent] of Object.entries(agents)) {
             if (agent.shortID == currID) {
@@ -217,14 +271,65 @@ function createPriorityQueue() {
             }
         }
 
-        priorityQueue.push(currID);
+        // Check if priority is relevant for the current agent
+        if (hasPriority.toLowerCase() == 'f') {
+            groupedPriority.push(currID);
+        }
+        else if (hasPriority.toLowerCase() == 't') {
+            priorityQueue.push(currID);
+        }
+        else {
+            console.log('One or more of the "Priority On/Off" letters is not "t" or "f"');
+            process.exit(1);
+        }
+    }
+
+    // At the end of the priority queue comes all of the people that will get
+    // tickets distrubuted equally
+    priorityQueue.push(groupedPriority);
+}
+
+// Check to make sure all IDs are used exactly once
+function checkOrderedIDs() {
+    let IDsUsedCorrectly = true;
+    
+    let data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+    let currData;
+    let IDs1 = [];
+    let IDs2 = [];
+    for (let i = 1; i < data.length; i++) {
+        currData = data[i];
+        IDs1.push(parseInt(currData[1]));
+        IDs2.push(parseInt(currData[2]));
+    }
+
+    if (IDs1.length != IDs2.length) {
+        IDsUsedCorrectly = false;
+    }
+
+    IDs1.sort();
+    IDs2.sort();
+    for (let i = 0; i < IDs1.length; i++) {
+        if (IDs1[i] != IDs2[i]) {
+            IDsUsedCorrectly = false;
+        }
+    }
+
+    if (!IDsUsedCorrectly) {
+        console.log("You did not use every ID exactly once. Please check columns B and C.");
+        process.exit(1);
     }
 }
 
 // Get the department ID from the Excel sheet
 function setDepartmentID() {
     let data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-    departmentId = data[1][9];
+    departmentId = data[1][10];
+
+    if (departmentId == null) {
+        console.log("Invalid department ID");
+        process.exit(1);
+    }
 }
 
 main();
